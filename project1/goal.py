e4c1bf4490db0ad3bdc82a98e59620c11643e8a3
ztstar <zoutong2006@gmail.com>
2026-03-11 10:18:59 -0500

    task 3

--- project1/goal.py
+++ project1/goal.py
@@ -65,6 +65,37 @@ class GraspGoal(Goal):
         """
         ########## TODO ##########
         
+        state_vec = state["stateVec"]
+
+        joint_values = state_vec[:7]
+
+        # target cube pose
+        x_target, y_target, theta_target = state_vec[7:10]
+        target_xy = np.array([x_target, y_target])
+
+        # end-effector pose
+        ee_pos, ee_quat = self.jac_solver.forward_kinematics(joint_values)
+        ee_xy = np.array([ee_pos[0] - 0.4, ee_pos[1] - 0.2])
+        
+        # quaternion -> yaw -> planar direction
+        x, y, z, w = ee_quat
+        ee_theta = np.arctan2(2 * (w*z + x*y), 1 - 2*(y*y + z*z))
+        ee_dir = np.array([np.cos(ee_theta), np.sin(ee_theta)])
+
+        diff = target_xy - ee_xy
+
+        d2 = abs(np.dot(diff, ee_dir))
+        d1 = abs(diff[0]*ee_dir[1] - diff[1]*ee_dir[0])
+
+        def wrap_to_pi(angle):
+            return (angle + np.pi) % (2 * np.pi) - np.pi
+
+        gamma = min(
+            abs(wrap_to_pi(ee_theta - (theta_target + k * np.pi / 2)))
+            for k in range(4)
+        )
+
+        return (d1 < 0.01) and (d2 < 0.02) and (gamma < 0.2)
 
         ##########################
         
